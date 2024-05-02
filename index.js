d8bd6c876a5d4539aa0c89eb9b4dc0fd7aa2fd3f
Philip Karanja <kpk.my.mailings@gmail.com>
2024-05-01 20:54:08 -0500

    Learning testing

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-const prompts = require('prompts');
+import prompts from 'prompts';
+
+var todoList = [];
 
 const main = async () => {
-    const nextTodo = async() => {
+    const todoMenu = async() => {
         return await prompts ({
-            type: 'text',
+            type: 'select',
             name: 'value',
-            message:'What is your next todo item?'
+            message:'What action to perform?',
+            choices: [
+                {title: "Show todo list", value: "S"},
+                {title: "Add todo list", value: "A"},
+                {title: "Show todo list", value: "D"},
+                {title: "Quit todo list", value: "Q"}
+
+            ],
         })
     }    
 
-    var message = await nextTodo();
-    console.log(message);
+    const addTodo = async() => {
+        return await prompts ({
+            type: 'text',
+            name: 'value',
+            message: 'What is your new todo item?',
+        })
 
-}
+    };
+    // var message = await nextTodo();
+    // console.log(message);
+  
+    const showTodo = () => {
+        console.log("========================");
+            todoList.forEach( (x) => console.log(x.value)) 
+        console.log("========================");
+        
+    };
+
+    var option = {};
+    do {
+        option = await todoMenu();
+        switch(option.value) {
+            case 'S' :
+                showTodo();
+                break;
+            case 'A':
+                todoList.push(await addTodo());
+                break;
+        }
+
+    } while(option.value != 'Q');
+};
 
 main().catch(console.error);
